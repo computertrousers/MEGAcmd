ea2ecd5502f997edaf3607969f2eaeb4cff8b3c3
Rich B <info@computertrousers.co.uk>
2021-12-03 21:12:06 +0000

    Update ubuntu.sh

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -5,7 +5,7 @@ DIRNAME=$(dirname $0)
 pushd ${DIRNAME}/..
 
 sudo apt update
-sudo apt install -y make libcrypto++ libpcrecpp0v5 libc-ares-dev zlib1g-dev libuv1 libssl-dev libsodium-dev readline-common sqlite3 curl autoconf libtool g++ libcrypto++-dev libz-dev libsqlite3-dev libssl-dev libcurl4-gnutls-dev libreadline-dev libpcre++-dev libsodium-dev libc-ares-dev libfreeimage-dev libavcodec-dev libavutil-dev libavformat-dev libswscale-dev libmediainfo-dev libzen-dev
+sudo apt install -y make autoconf libtool g++ libcrypto++-dev libz-dev libsqlite3-dev libssl-dev libcurl4-gnutls-dev libreadline-dev libpcre++-dev libsodium-dev libc-ares-dev libfreeimage-dev libavcodec-dev libavutil-dev libavformat-dev libswscale-dev libmediainfo-dev libzen-dev libuv1-dev
 
 git submodule update --init --recursive
 
